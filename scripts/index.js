c753c3b70524fd5b955df852b3aed9dc7a4bb8cb
Adrian A. <a.adrian.tech@googlemail.com>
2026-09-19 03:11:17 +0200

    Handle DOM/JSON edge cases (#67)

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import Root from './components/Root';
 
-render(
-  <Root />,
-  document.evaluate('//div[@class="main-sidebar"]/div[@class="sidebar"]/ul[@class="sidebar-menu"]/li[contains(.,"Files")]',
-    document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
-);
+const mountNode = document.evaluate('//div[@class="main-sidebar"]/div[@class="sidebar"]/ul[@class="sidebar-menu"]/li[contains(.,"Files")]',
+  document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+if (mountNode) {
+  render(<Root />, mountNode);
+}
